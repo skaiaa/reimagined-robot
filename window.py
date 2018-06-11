4c1e7cfdce21b5c3ad8106bc2c06947ff6a97671
Ania Przybycień <ania.przybycien@gmail.com>
2018-06-12 00:15:58 +0200

    layout

--- window.py
+++ window.py
@@ -1,13 +1,14 @@
 import tkinter as tk
 from virtual_world.gui import Dialog
+from tkinter import scrolledtext
 
 
 class WorldSizeDialog(Dialog):
     def __init__(self, parent, title):
-        super(WorldSizeDialog, self).__init__(parent, title)
         self.e1 = None
         self.e2 = None
         self.result = None
+        super(WorldSizeDialog, self).__init__(parent, title)
 
     def body(self, master):
         tk.Label(master, text="X:").grid(row=0)
@@ -31,7 +32,7 @@ class WorldSizeDialog(Dialog):
 
 
 class Application(tk.Frame):
-    def __init__(self, master=None):
+    def __init__(self, master,dimentions):
         super().__init__(master)
         self.pack()
         self.quit_btn = None
@@ -39,25 +40,69 @@ class Application(tk.Frame):
         self.load_btn = None
         self.new_game_btn = None
         self.next_turn_btn = None
-        self.create_widgets()
+        self.logging_stext = None
+        self.create_widgets(master)
+        self.create_board(master, dimentions)
 
-    def create_widgets(self):
-        self.save_btn = tk.Button(self, text="SAVE", command=self.say_hi)
-        self.save_btn.pack(side="left")
-        self.load_btn = tk.Button(self, text="LOAD", command=self.say_hi)
-        self.load_btn.pack(side="left")
-        self.new_game_btn = tk.Button(self, text="NEW GAME", command=self.say_hi)
-        self.new_game_btn.pack(side="left")
-        self.next_turn_btn = tk.Button(self, text="NEXT TURN", command=self.say_hi)
-        self.next_turn_btn.pack(side="left")
-        self.quit_btn = tk.Button(self, text="QUIT", fg="red", command=root.destroy)
-        self.quit_btn.pack(side="left")
+    def create_widgets(self, window):
+        widget_frame = tk.Frame(window)
+        self.save_btn = tk.Button(widget_frame, text="SAVE", command=self.say_hi)
+        self.save_btn.pack(side="top", fill="x")
+        self.load_btn = tk.Button(widget_frame, text="LOAD", command=self.say_hi)
+        self.load_btn.pack(side="top", fill="x")
+        self.new_game_btn = tk.Button(widget_frame, text="NEW GAME", command=self.say_hi)
+        self.new_game_btn.pack(side="top", fill="x")
+        self.next_turn_btn = tk.Button(widget_frame, text="NEXT TURN", command=self.say_hi)
+        self.next_turn_btn.pack(side="top", fill="x")
+        self.quit_btn = tk.Button(widget_frame, text="QUIT", fg="red", command=root.destroy)
+        self.quit_btn.pack(side="top", fill="x")
+        self.logging_stext = scrolledtext.ScrolledText(widget_frame, state="disabled")
+        self.logging_stext.configure(font="TkDefaultFont")
+        self.logging_stext.tag_config("INFO", foreground="black")
+        self.logging_stext.tag_config("DEBUG", foreground="gray")
+        self.logging_stext.tag_config("WARNING", foreground="orange")
+        self.logging_stext.tag_config("ERROR", foreground="red")
+        self.logging_stext.tag_config("CRITICAL", foreground="red", underline=1)
+        self.logging_stext.pack(side="bottom")
+        widget_frame.pack(side="left")
+
+    def create_board(self, window, dimentions):
+        game_frame = tk.Frame(window, borderwidth=2, relief=tk.SUNKEN)
+
+        def create_square(i, j):
+            f = tk.Frame(game_frame, height=30, width=30)
+            s = tk.Button(f, borderwidth=1, state="normal",
+                          disabledforeground="#000000")
+            s.pack(fill=tk.BOTH, expand=True)
+
+            # buttons bindings
+            def __handler(event, x=i, y=j):
+                pass
+                # if event.num == 1:
+                #     handlers.left_handler(GRID, BOARD, i, j, mine)
+                # elif event.num == 3:
+                #     handlers.right_handler(GRID, BOARD, i, j, flag)
+                # else:
+                #     raise Exception('Invalid event code.')
+
+            s.bind("<Button-1>", __handler)
+            s.bind("<Button-3>", __handler)
+
+            f.pack_propagate(False)
+            f.grid(row=i, column=j)
+            return s
+
+        BOARD = [[create_square(i, j) for j in range(dimentions[0])]
+                 for i in range(dimentions[1])]
+        game_frame.pack(padx=10, pady=10, side="right")
+        return BOARD
 
     def say_hi(self):
         print("hi there, everyone!")
 
 
 root = tk.Tk()
-app = Application(master=root)
 dialog = WorldSizeDialog(root, "Enter world size")
+app = Application(root, dialog.result)
+
 app.mainloop()
